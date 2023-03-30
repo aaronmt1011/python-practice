1ca7fc346c584d3fffac5697ea787f626184b729
aaronmt1011 <aaronmt1011@gmail.com>
2023-03-30 09:49:20 -0600

    Changed title to reflect true nature of program

--- intro-practice/temp-convert/README.md
+++ intro-practice/temp-convert/README.md
@@ -1,2 +1,2 @@
-<h1>Temperature Conversion Program:</h1>
+<h1>Temperature Conversion Program - Celsius to Fahrenheit:</h1>
 <p>This file will focus on converting temperatures from celsius to fahrenheit.</p>
--- intro-practice/temp-convert/temp-convert.py
+++
@@ -1,9 +0,0 @@
-# temp-convert.py
-# A simple program to convert celsius to fahrenheit 
-
-def main():
-    celsius = eval(input("Please enter °C (celsius) temperature: "))
-    fahrenheit = 9/5 * celsius + 32
-    print("The temperature is: ", fahrenheit, "°F")
-
-main()
