30d3a2659b56a230507c81b8cf4c8c8bf671e942
aaronmt1011 <aaronmt1011@gmail.com>
2023-04-03 07:20:02 -0600

    Program now computes avg of 3 numbers

--- intro-practice/avg/avg.py
+++ intro-practice/avg/avg.py
@@ -2,10 +2,10 @@
 # This program gives the average of the 2 input numbers
 
 def main():
-    print("This program computes the average of 2 numbers")
+    print("This program computes the average of 3 numbers")
 
-    num1, num2 = eval(input("Enter two numbers separated by a comma: "))
-    average = (num1 + num2) / 2
-    print("The average of the 2 numbers is: ", average)
+    num1, num2, num3 = eval(input("Enter three numbers separated by a comma: "))
+    average = (num1 + num2 + num3) / 3
+    print("The average of the 3 numbers is: ", average)
 
 main()
