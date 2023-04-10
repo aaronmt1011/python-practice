3fbd9245069fac954f43d75e657e5516b817cf3b
aaronmt1011 <aaronmt1011@gmail.com>
2023-04-10 08:14:17 -0600

    Created basic interface for user

---
+++ numerical-practice/factorial/factorial.py
@@ -0,0 +1,14 @@
+# factorial.py
+# This program receives an inputted number and outputs a factorial
+
+def factorial():
+    print("This program is a factorial program.")
+    print("It will give out a factorial for any inputted number.")
+    print("")
+    
+    num = int(input("Please enter number: "))
+    
+
+    print("Here is num: ", num)
+
+factorial()
