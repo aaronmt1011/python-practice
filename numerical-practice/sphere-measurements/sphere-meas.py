01880990a942f3c4b1f57dc53c6586cb0fcbfdde
aaronmt1011 <aaronmt1011@gmail.com>
2023-04-12 16:57:58 -0600

    Created basic interface

---
+++ numerical-practice/sphere-measurements/sphere-meas.py
@@ -0,0 +1,12 @@
+# sphere-meas.py
+# This program shows results for various sphere measurements
+
+def sphere():
+    print("This program will provide some sphere measurements.")
+    print("All you have to do is provide the radius of the sphere.")
+    print("")
+    n = int(input("Please enter the radius of the sphere: "))
+
+    print("Here is the radius", n)
+
+sphere()
