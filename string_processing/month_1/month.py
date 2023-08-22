21e59e1e196312cc8f494c72d3926fd411a4166a
aaronmt1011 <aaronmt1011@gmail.com>
2023-08-22 13:14:17 -0600

    Changed some comments and some var names

--- string_processing/month_1/month.py
+++ string_processing/month_1/month.py
@@ -1,5 +1,5 @@
 # month.py
-# This program gives out the month.
+# This program gives out the months abbreviation.
 
 def month():
     months = "JanFebMarAprMayJunJulAugSepOctNovDec"
@@ -7,9 +7,9 @@ def month():
     print("This program gives out the month of the year!")
     print("All you have to input is the months number (1-12) \n")
 
-    n = int(input("Please enter the month number (1-12): "))
-    pos = (n -1) * 3 
-    month = months[pos:pos+3]
-    print("\n" + "The month is", month + "!")
+    m1 = int(input("Please enter the month number (1-12): "))
+    pos = (m1 - 1) * 3 
+    mAbbrev = months[pos:pos+3]
+    print("\n" + "The month is", mAbbrev + "!")
 
 month()
