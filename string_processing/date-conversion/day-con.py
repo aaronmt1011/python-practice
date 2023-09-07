656ce27a913f9ebbb9211f6dd94e959410926687
aaronmt1011 <aaronmt1011@gmail.com>
2023-09-07 11:27:41 -0600

    Used .format() to simplify last 2 lines into 1 single line

--- string_processing/date-conversion/day-con.py
+++ string_processing/date-conversion/day-con.py
@@ -6,13 +6,11 @@ def day():
     print("This program recives your date input and formats it differently!\n")
     dNum = input("Enter a date (mm/dd/yyyy): ")
 
-    mm, dd, yy = dNum.split("/")
+    m, d, y = dNum.split("/")
 
     month = ["January", "February", "March", "April", "May", "June", "July", 
               "August", "September", "October", "November", "December"]
 
-    mm = month[int(mm)-1]
-
-    print("\nThe converted date is:", mm, dd + ",", yy)
+    print("\nThe converted date is: {0} {1}, {2}".format(month[int(m)-1], int(d), int(y)))
     
 day()
