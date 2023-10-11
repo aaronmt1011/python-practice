392ce6c5c89bf738644b1c74c156fcb21628fdb6
aaronmt1011 <aaronmt1011@gmail.com>
2023-10-11 11:16:36 -0600

    Added count(), sectionread(), and startswith() functions to file

--- reading_files/text-reader/reader.py
+++ reading_files/text-reader/reader.py
@@ -1,6 +1,34 @@
 # reader.py
 # This program reads a file and does cool things to it
 
+# count() recieves fileHand (text file) and gives out the line count
+def count(fileHand):
+    count = 0
+    for line in fileHand:
+        count = count + 1
+
+    print("\nTotal line Count: " , count)
+
+
+
+# sectionread() displays only a portion of the files text
+def sectionread(file, x, y):
+    print(file[x:y])
+
+
+
+# startswith() searches for lines that start with "From: " and displays them
+def startswith(fileHand):
+    for line in fileHand:
+        # rstrip() is used to get rid of white space (\n) from text file
+        line = line.rstrip()
+        if line.startswith('From:') :
+            print(line)
+
+
+
+# read() is the main program body
+# functions from above will be ran through read()
 def read():
     print("This program reads a file and does cool stuff with it. There is \nalso a default text file that is included within the project folder \nthat can be used. You can also input your own text file. Just \nmake sure to place own file in the same location as the reader.py.\n")
     print("Please answer the following question with a 'y' or 'n'.")
@@ -10,9 +38,7 @@ def read():
         answer = input("Do you want to use the given text file? ")
     
     if answer == "y":
-        fileHand = open('repos/python-practice/reading_files/text-reader/mbox.txt')
-    
-    
+        fileHand = open('repos/python-practice/reading_files/text-reader/mbox.txt')   
     elif answer == "n":
         print("\nMake sure to have file path for own text file for the next question. \nFor example the location of the default text file was: \nrepos/python-practice/reading_files/text-reader/mbox.txt")
         while True:
@@ -22,9 +48,10 @@ def read():
             except FileNotFoundError:
                 print("\nPlease input the files path CORRECTLY next time.")
 
-    # The following code is the easiest way to read through an entire ext file
-    for lines in fileHand:
-        print(lines)
+    # count(fileHand)
+    # startswith(fileHand)
+    file = fileHand.read()
 
+    sectionread(file, 0, 20)
 
 read()
